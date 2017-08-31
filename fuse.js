017f25ffc759f62bb1a7495cfbe8911246ff397b
Robin Schmitz <r.schmitz@kapilendo.de>
2017-08-31 10:48:01 +0200

    updating fuse, simplifying css processing

--- fuse.js
+++ fuse.js
@@ -1,25 +1,31 @@
 const argv = require('yargs').argv;
-const { BabelPlugin, CSSPlugin, EnvPlugin, FuseBox, PostCSSPlugin, SassPlugin, UglifyJSPlugin } = require('fuse-box');
+const {
+    BabelPlugin,
+    CSSPlugin,
+    EnvPlugin,
+    FuseBox,
+    PostCSSPlugin,
+    SassPlugin,
+    UglifyJSPlugin
+} = require('fuse-box');
 const path = require('path');
 
 const production = process.env.NODE_ENV === 'production';
-const plugins = production 
-    ? [require('autoprefixer'), require('cssnano'), require('lost')]
-    : [require('lost')];
 
 const fuse = FuseBox.init({
     homeDir: '',
     output: 'public/$name.js',
     plugins: [
-        [ 
-            SassPlugin(), 
-            PostCSSPlugin(plugins),
-            CSSPlugin({ 
-                outFile: file => `./public/${file}`, 
+        [
+            SassPlugin(),
+            CSSPlugin({
+                outFile: file => `./public/${file}`,
                 inject: production ? false : (file) => `/${file}`
             })
         ],
-        EnvPlugin({ NODE_ENV: production ? 'production' : 'development' }),
+        EnvPlugin({
+            NODE_ENV: production ? 'production' : 'development'
+        }),
         BabelPlugin(),
         production && UglifyJSPlugin()
     ]
@@ -38,7 +44,7 @@ fuse.bundle('bundle')
     .target('browser')
     .watch(`(index.js|styles/**.scss|styles/**/**.scss)`)
     .hmr()
-    .instructions(`> [index.js]`);   
+    .instructions(`> [index.js]`);
 
 
 fuse.run();
